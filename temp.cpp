8151fd358531be6e54e1e1f6eb666fce60256e3c
Devadutta Hati <devhati83@gmail.com>
2022-01-11 16:02:28 +0530

    Template

---
+++ temp.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+typedef long long int ll;
+typedef vector<int> vii;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<long long, long long>> vpll;
+ 
+#define all(x)                (x).begin(), (x).end() 
+#define rep(i, a, n)          for (ll i = a; i < n; i++)
+#define pb                    push_back
+#define popb                  pop_back
+#define mp                    make_pair
+#define fi                    first
+#define se                    second
+#define nl                    '\n'
+#define fix(prec)             {cout << setprecision(prec) << fixed;}
+#define debug(x) cout<< #x << " "<<x<<endl;
+const ll modlow = 998244353;
+const ll modup = 1000000000 + 7;
+const ll N = 10000000 + 6;
+const double pi = acos(-1);
+// ctrl+q to stop build 
+//ctrl+shift+b to start build
+ 
+      //global vector
+void flagout(int i){
+    if (i==0)
+    {
+        cout<<"NO"<<nl;
+    }else if(i==1){
+        cout<<"YES"<<nl;
+    }
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  
+ 
+  int t;
+  cin >> t;
+  while(t--){
+    
+  }
+return 0;
+}
